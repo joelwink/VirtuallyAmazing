68a0b49b5a258d342799289de81a271f9823a373
Rik Faith <nutanixrik@users.noreply.github.com>
2018-05-08 12:53:34 -0400

    Script determines CPU load

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 # script.py -*-python-*-
 
 import json
+import requests
 import sys
 sys.path.append("./automation/solution/")
 
@@ -32,6 +33,20 @@ class Connector(object):
     self._show_list(VirtualMachineService().getVMS(self.connection,
                                                    self.data))
 
+    try:
+      r = requests.get(
+        'https://10.21.72.37:9440/PrismGateway/services/rest/v2.0/cluster/',
+        json=self.data,
+        verify=False,
+        auth=(USER, PASSWD))
+
+      r.raise_for_status()
+    except requests.HTTPError as ex:
+      print "Failed to create %s\n" % str(ex)
+
+    j = r.json()
+    print 'Load = %.3f%%' % (int(j['stats']['hypervisor_cpu_usage_ppm']) /
+                             1000000.0 * 100.0)
 
 if __name__ == "__main__":
   c = Connector()
